5c55aa0cb2357e11282c8913da4b5899dab21c7d
Jan <mig@jankjr.dk>
2023-04-22 11:55:37 +0100

    keep package.lock in sync with package.json on publish

--- publish.sh
+++ publish.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Ensure no uncommitted changes
-git diff-index --quiet HEAD -- || { echo "Uncommitted changes detected. Aborting."; exit 1; }
+git diff-index --quiet HEAD -- || {
+  echo "Uncommitted changes detected. Aborting."
+  exit 1
+}
 
 # Read current version
 current_version=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
-
 
 # Ask user for new version and message
 read -p "New version (current: $current_version): " new_version
@@ -13,6 +15,14 @@ read -p "Tag message: " tag_message
 
 # Update package.json with new version
 sed -i '' "s/\($current_version\).*/$new_version\",/" package.json
+
+if npm install; then
+  echo "package.json and package.lock updated"
+else
+  echo "npm install failed. Aborting."
+  exit 1
+fi
+
 git add package.json
 git commit -m "Bump package.json version to '$new_version'"
 
@@ -37,14 +47,14 @@ if npm run build && npm run test; then
   npm publish
 else
   echo "Error: Build or test failed. Aborting."
-  
+
   # Go back to previous branch and delete the release branch
   git checkout -
   git branch -D $release_branch
-  
+
   # Delete the tag
   git tag -d "v$new_version"
-  
+
   # Revert the package.json version bump
   git reset HEAD^
   git checkout package.json
@@ -54,5 +64,5 @@ fi
 # Go back to previous branch
 git checkout -
 
-# Delete leftover artifact directories 
+# Delete leftover artifact directories
 npm run clean

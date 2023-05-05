c642468b4554b0708ec2ed4c5c1a3d3889299d72
Jan <mig@jankjr.dk>
2023-05-05 17:48:52 +0100

    Release v1.5.3 binaries

---
+++ lib.commonjs/action/SATokens.js
@@ -0,0 +1,88 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BurnSATokensAction = exports.MintSATokensAction = void 0;
+const contracts_1 = require("../contracts");
+const utils_1 = require("../base/utils");
+const Action_1 = require("./Action");
+const ContractCall_1 = require("../base/ContractCall");
+const Approval_1 = require("../base/Approval");
+const ray = 10n ** 27n;
+const halfRay = ray / 2n;
+const rayMul = (a, b) => {
+    return (halfRay + a * b) / ray;
+};
+function rayDiv(a, b) {
+    const halfB = b / 2n;
+    return (halfB + a * ray) / b;
+}
+const saTokenInterface = contracts_1.IStaticATokenLM__factory.createInterface();
+class MintSATokensAction extends Action_1.Action {
+    universe;
+    underlying;
+    saToken;
+    rate;
+    gasEstimate() {
+        return BigInt(300000n);
+    }
+    async encode([amountsIn], destination) {
+        return new ContractCall_1.ContractCall((0, utils_1.parseHexStringIntoBuffer)(saTokenInterface.encodeFunctionData('deposit', [
+            destination.address,
+            amountsIn.amount,
+            0,
+            true,
+        ])), this.saToken.address, 0n, this.gasEstimate(), `Mint(${this.saToken}, input: ${amountsIn}, destination: ${destination})`);
+    }
+    async quote([amountsIn]) {
+        await this.universe.refresh(this.address);
+        return [
+            this.saToken.fromBigInt(rayDiv(amountsIn.into(this.saToken).amount, this.rate.value)),
+        ];
+    }
+    constructor(universe, underlying, saToken, rate) {
+        super(saToken.address, [underlying], [saToken], Action_1.InteractionConvention.ApprovalRequired, Action_1.DestinationOptions.Recipient, [new Approval_1.Approval(underlying, saToken.address)]);
+        this.universe = universe;
+        this.underlying = underlying;
+        this.saToken = saToken;
+        this.rate = rate;
+    }
+    toString() {
+        return `SATokenMint(${this.saToken.toString()})`;
+    }
+}
+exports.MintSATokensAction = MintSATokensAction;
+class BurnSATokensAction extends Action_1.Action {
+    universe;
+    underlying;
+    saToken;
+    rate;
+    gasEstimate() {
+        return BigInt(300000n);
+    }
+    async encode([amountsIn], destination) {
+        return new ContractCall_1.ContractCall((0, utils_1.parseHexStringIntoBuffer)(saTokenInterface.encodeFunctionData('withdraw', [
+            destination.address,
+            amountsIn.amount,
+            true,
+        ])), this.saToken.address, 0n, this.gasEstimate(), 'Burn ' + this.saToken.name);
+    }
+    async quote([amountsIn]) {
+        await this.universe.refresh(this.address);
+        return [
+            this.saToken
+                .fromBigInt(rayMul(amountsIn.amount, this.rate.value))
+                .into(this.underlying),
+        ];
+    }
+    constructor(universe, underlying, saToken, rate) {
+        super(saToken.address, [saToken], [underlying], Action_1.InteractionConvention.None, Action_1.DestinationOptions.Recipient, []);
+        this.universe = universe;
+        this.underlying = underlying;
+        this.saToken = saToken;
+        this.rate = rate;
+    }
+    toString() {
+        return `SATokenBurn(${this.saToken.toString()})`;
+    }
+}
+exports.BurnSATokensAction = BurnSATokensAction;
+//# sourceMappingURL=SATokens.js.map
---
+++ lib.commonjs/searcher/Swap.d.ts
@@ -0,0 +1,56 @@
+import { type Action } from '../action/Action';
+import { type Address } from '../base/Address';
+import { Token, TokenAmounts, type TokenQuantity } from '../entities/Token';
+import { type Universe } from '../Universe';
+/**
+ * A single Step token exchange
+ */
+export declare class SingleSwap {
+    readonly input: TokenQuantity[];
+    readonly action: Action;
+    readonly output: TokenQuantity[];
+    constructor(input: TokenQuantity[], action: Action, output: TokenQuantity[]);
+    exchange(tokenAmounts: TokenAmounts): Promise<void>;
+    toString(): string;
+}
+/**
+ * A SwapPath groups a set of SingleSwap's together. The output of one SingleSwap is the input of the next.
+ * A SwapPath may be optimized, as long as the input's and output's remain the same.
+ */
+export declare class SwapPath {
+    readonly universe: Universe;
+    readonly inputs: TokenQuantity[];
+    readonly steps: SingleSwap[];
+    readonly outputs: TokenQuantity[];
+    readonly outputValue: TokenQuantity;
+    readonly destination: Address;
+    constructor(universe: Universe, inputs: TokenQuantity[], steps: SingleSwap[], outputs: TokenQuantity[], outputValue: TokenQuantity, destination: Address);
+    exchange(tokenAmounts: TokenAmounts): Promise<void>;
+    compare(other: SwapPath): number;
+    toString(): string;
+    describe(): string[];
+}
+/**
+ * SwapPaths groups SwapPath's together into sections
+ */
+export declare class SwapPaths {
+    readonly universe: Universe;
+    readonly inputs: TokenQuantity[];
+    readonly outputs: TokenQuantity[];
+    readonly outputValue: TokenQuantity;
+    readonly destination: Address;
+    readonly swapPaths: SwapPath[];
+    constructor(universe: Universe, inputs: TokenQuantity[], swapPaths: SwapPath[], outputs: TokenQuantity[], outputValue: TokenQuantity, destination: Address);
+    exchange(tokenAmounts: TokenAmounts): Promise<void>;
+    toString(): string;
+    describe(): string[];
+}
+/** Abstract set of steps to go from A to B */
+export declare class SwapPlan {
+    readonly universe: Universe;
+    readonly steps: Action[];
+    constructor(universe: Universe, steps: Action[]);
+    get inputs(): readonly Token[];
+    quote(input: TokenQuantity[], destination: Address): Promise<SwapPath>;
+    toString(): string;
+}

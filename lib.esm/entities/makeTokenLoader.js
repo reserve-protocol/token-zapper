8eb79d90961da2b33cb562ad5a9156cab1c1f794
Jan <mig@jankjr.dk>
2023-09-19 14:06:43 +0100

    Release v2.0.1-dev-16 binaries

---
+++ lib.esm/entities/makeTokenLoader.js
@@ -0,0 +1,31 @@
+import { parseHexStringIntoBuffer } from '../base/utils';
+import { id } from "@ethersproject/hash";
+import { defaultAbiCoder } from "@ethersproject/abi";
+import { IERC20__factory } from '../contracts/factories/IERC20__factory';
+export const makeTokenLoader = (provider) => async (address) => {
+    const erc20 = IERC20__factory.connect(address.address, provider);
+    let [symbol, decimals] = await Promise.all([
+        provider.call({
+            to: address.address,
+            data: id('symbol()').slice(0, 10),
+        }),
+        erc20.decimals().catch(() => 0),
+    ]);
+    if (symbol.length === 66) {
+        let buffer = parseHexStringIntoBuffer(symbol);
+        let last = buffer.indexOf(0);
+        if (last == -1) {
+            last = buffer.length;
+        }
+        buffer = buffer.subarray(0, last);
+        symbol = buffer.toString('utf8');
+    }
+    else {
+        symbol = defaultAbiCoder.decode(['string'], symbol)[0];
+    }
+    return {
+        symbol,
+        decimals,
+    };
+};
+//# sourceMappingURL=makeTokenLoader.js.map

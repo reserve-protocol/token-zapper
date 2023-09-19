c69ebd64a8197976da3a85d1f4130b8498fc0b94
Jan <mig@jankjr.dk>
2023-09-19 21:54:53 +0100

    Release v2.1.0-rc-8 binaries

---
+++ types/action/Comet.d.ts
@@ -0,0 +1,26 @@
+import { type Universe } from '../Universe';
+import { type Address } from '../base/Address';
+import { ContractCall } from '../base/ContractCall';
+import { type Token, type TokenQuantity } from '../entities/Token';
+import { Action } from './Action';
+export declare class MintCometAction extends Action {
+    readonly universe: Universe;
+    readonly baseToken: Token;
+    readonly receiptToken: Token;
+    gasEstimate(): bigint;
+    encode([amountsIn]: TokenQuantity[]): Promise<ContractCall>;
+    quote([amountsIn]: TokenQuantity[]): Promise<TokenQuantity[]>;
+    constructor(universe: Universe, baseToken: Token, receiptToken: Token);
+    toString(): string;
+}
+export declare class BurnCometAction extends Action {
+    readonly universe: Universe;
+    readonly baseToken: Token;
+    readonly receiptToken: Token;
+    gasEstimate(): bigint;
+    encode([amountsIn]: TokenQuantity[], dest: Address): Promise<ContractCall>;
+    quote([amountsIn]: TokenQuantity[]): Promise<TokenQuantity[]>;
+    constructor(universe: Universe, baseToken: Token, receiptToken: Token);
+    toString(): string;
+}
+//# sourceMappingURL=Comet.d.ts.map

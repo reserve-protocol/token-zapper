09ef9283d92cf18703ddeb026a0e5559d397826c
Jan <mig@jankjr.dk>
2023-10-09 18:36:09 +0200

    Release v2.5.4 binaries

---
+++ lib.commonjs/curve-js/src/factory/constants.js
@@ -0,0 +1,133 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FACTORY_CONSTANTS = exports.basePoolIdZapDictEthereum = exports.implementationBasePoolIdDictEthereum = exports.implementationABIDictEthereum = void 0;
+const utils_1 = require("../constants/utils");
+const factorySwapABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factoryPools/swap.json"))).then(i => i.default);
+const MetaUSDABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaUSD.json"))).then(i => i.default);
+const MetaUSDBalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaUSDBalances.json"))).then(i => i.default);
+const MetaFraxUSDABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaFraxUSD.json"))).then(i => i.default);
+const MetaFraxUSDBalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaFraxUSDBalances.json"))).then(i => i.default);
+const MetaBTCABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaBTC.json"))).then(i => i.default);
+const MetaBTCBalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaBTCBalances.json"))).then(i => i.default);
+const MetaBTCRenABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaBTCRen.json"))).then(i => i.default);
+const MetaBTCRenBalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaBTCBalancesRen.json"))).then(i => i.default);
+const MetaSbtc2ABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaSbtc2.json"))).then(i => i.default);
+const MetaSbtc2BalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/MetaSbtc2Balance.json"))).then(i => i.default);
+const Plain2BasicABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain2Basic.json"))).then(i => i.default);
+const Plain2BalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain2Balances.json"))).then(i => i.default);
+const Plain2ETHABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain2ETH.json"))).then(i => i.default);
+const Plain2OptimizedABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain2Optimized.json"))).then(i => i.default);
+const Plain3BasicABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain3Basic.json"))).then(i => i.default);
+const Plain3BalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain3Balances.json"))).then(i => i.default);
+const Plain3ETHABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain3ETH.json"))).then(i => i.default);
+const Plain3OptimizedABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain3Optimized.json"))).then(i => i.default);
+const Plain4BasicABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain4Basic.json"))).then(i => i.default);
+const Plain4BalancesABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain4Balances.json"))).then(i => i.default);
+const Plain4ETHABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain4ETH.json"))).then(i => i.default);
+const Plain4OptimizedABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factory-v2/Plain4Optimized.json"))).then(i => i.default);
+// --- ZAPS --
+const factoryDepositABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/factoryPools/deposit.json"))).then(i => i.default);
+const fraxusdcMetaZapABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/fraxusdc/meta_zap.json"))).then(i => i.default);
+const RenMetaZapABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/ren/meta_zap.json"))).then(i => i.default);
+const Sbtc2MetaZapABI = () => Promise.resolve().then(() => __importStar(require("../constants/abis/sbtc2/meta_zap.json"))).then(i => i.default);
+exports.implementationABIDictEthereum = (0, utils_1.lowerCaseKeys)({
+    "0x5F890841f657d90E081bAbdB532A05996Af79Fe6": factorySwapABI,
+    "0x213be373FDff327658139C7df330817DAD2d5bBE": MetaUSDABI,
+    "0x55Aa9BF126bCABF0bDC17Fa9E39Ec9239e1ce7A9": MetaUSDBalancesABI,
+    "0x33bB0e62d5e8C688E645Dd46DFb48Cd613250067": MetaFraxUSDABI,
+    "0x2EB24483Ef551dA247ab87Cf18e1Cc980073032D": MetaFraxUSDBalancesABI,
+    "0xF9B62b61d108232Ef0C9DD143bb3c22c7D4A715a": MetaFraxUSDABI,
+    "0xB172AC2Fe440B5dA74Dc460e5E9d96bc2BF6261F": MetaFraxUSDBalancesABI,
+    "0xC6A8466d128Fbfd34AdA64a9FFFce325D57C9a52": MetaBTCABI,
+    "0xc4C78b08fA0c3d0a312605634461A88184Ecd630": MetaBTCBalancesABI,
+    "0xECAaecd9d2193900b424774133B1f51ae0F29d9E": MetaBTCRenABI,
+    "0x40fD58D44cFE63E8517c9Bb3ac98676838Ea56A8": MetaBTCRenBalancesABI,
+    "0x008CFa89df5B0c780cA3462fc2602D7F8c7Ac315": MetaSbtc2ABI,
+    "0xAbc533EbCDdeD41215C46ee078C5818B5b0A252F": MetaSbtc2BalancesABI,
+    "0x6523Ac15EC152Cb70a334230F6c5d62C5Bd963f1": Plain2BasicABI,
+    "0x24D937143d3F5cF04c72bA112735151A8CAE2262": Plain2BalancesABI,
+    "0x6326DEbBAa15bCFE603d831e7D75f4fc10d9B43E": Plain2ETHABI,
+    "0x4A4d7868390EF5CaC51cDA262888f34bD3025C3F": Plain2OptimizedABI,
+    "0xc629a01eC23AB04E1050500A3717A2a5c0701497": Plain2BasicABI,
+    "0x94b4DFd9Ba5865Cc931195c99A2db42F3fc5d45B": Plain2ETHABI,
+    "0x9B52F13DF69D79Ec5aAB6D1aCe3157d29B409cC3": Plain3BasicABI,
+    "0x50b085f2e5958C4A87baf93A8AB79F6bec068494": Plain3BalancesABI,
+    "0x8c1aB78601c259E1B43F19816923609dC7d7de9B": Plain3ETHABI,
+    "0xE5F4b89E0A16578B3e0e7581327BDb4C712E44De": Plain3OptimizedABI,
+    "0x5Bd47eA4494e0F8DE6e3Ca10F1c05F55b72466B8": Plain4BasicABI,
+    "0xd35B58386705CE75CE6d09842E38E9BE9CDe5bF6": Plain4BalancesABI,
+    "0x88855cdF2b0A8413D470B86952E726684de915be": Plain4ETHABI,
+    "0xaD4753D045D3Aed5C1a6606dFb6a7D7AD67C1Ad7": Plain4OptimizedABI,
+});
+exports.implementationBasePoolIdDictEthereum = (0, utils_1.lowerCaseKeys)({
+    "0x5F890841f657d90E081bAbdB532A05996Af79Fe6": "3pool",
+    "0x213be373FDff327658139C7df330817DAD2d5bBE": "3pool",
+    "0x55Aa9BF126bCABF0bDC17Fa9E39Ec9239e1ce7A9": "3pool",
+    "0x33bB0e62d5e8C688E645Dd46DFb48Cd613250067": "fraxusdc",
+    "0x2EB24483Ef551dA247ab87Cf18e1Cc980073032D": "fraxusdc",
+    "0xF9B62b61d108232Ef0C9DD143bb3c22c7D4A715a": "fraxusdp",
+    "0xB172AC2Fe440B5dA74Dc460e5E9d96bc2BF6261F": "fraxusdp",
+    "0xC6A8466d128Fbfd34AdA64a9FFFce325D57C9a52": "sbtc",
+    "0xc4C78b08fA0c3d0a312605634461A88184Ecd630": "sbtc",
+    "0xECAaecd9d2193900b424774133B1f51ae0F29d9E": "ren",
+    "0x40fD58D44cFE63E8517c9Bb3ac98676838Ea56A8": "ren",
+    "0x008CFa89df5B0c780cA3462fc2602D7F8c7Ac315": "sbtc2",
+    "0xAbc533EbCDdeD41215C46ee078C5818B5b0A252F": "sbtc2",
+});
+exports.basePoolIdZapDictEthereum = {
+    '3pool': {
+        address: "0xA79828DF1850E8a3A3064576f380D90aECDD3359".toLowerCase(),
+        ABI: factoryDepositABI,
+    },
+    fraxusdc: {
+        address: "0x08780fb7E580e492c1935bEe4fA5920b94AA95Da".toLowerCase(),
+        ABI: fraxusdcMetaZapABI,
+    },
+    fraxusdp: {
+        address: "0x63B709d2118Ba0389ee75A131d1F9a473e06afbD".toLowerCase(),
+        ABI: fraxusdcMetaZapABI,
+    },
+    sbtc: {
+        address: "0x7abdbaf29929e7f8621b757d2a7c04d78d633834".toLowerCase(),
+        ABI: factoryDepositABI,
+    },
+    ren: {
+        address: "0x8Fb3Ec8f2d1Dc089E70CD61f1E49496d443B2124".toLowerCase(),
+        ABI: RenMetaZapABI,
+    },
+    sbtc2: {
+        address: "0xA2d40Edbf76C6C0701BA8899e2d059798eBa628e".toLowerCase(),
+        ABI: Sbtc2MetaZapABI,
+    },
+};
+exports.FACTORY_CONSTANTS = {
+    1: {
+        implementationABIDict: exports.implementationABIDictEthereum,
+        implementationBasePoolIdDict: exports.implementationBasePoolIdDictEthereum,
+        basePoolIdZapDict: exports.basePoolIdZapDictEthereum,
+    }
+};
+//# sourceMappingURL=constants.js.map
---
+++ types/configuration/base.d.ts
@@ -0,0 +1,70 @@
+import { type Universe } from '../Universe';
+export declare const COMMON_TOKENS: {
+    readonly USDC: "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA";
+    readonly USDbC: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+    readonly DAI: "0x50c5725949a6f0c72e6c4a641f24049a917db0cb";
+    readonly WETH: "0x4200000000000000000000000000000000000006";
+    readonly ERC20GAS: "0x4200000000000000000000000000000000000006";
+};
+export declare const RTOKENS: {
+    readonly testUSD: {
+        readonly main: "0x4A8A34eEFc4892BF2d663FFd85668E80d54c3fE6";
+        readonly erc20: "0x50249C768A6D3Cb4B6565c0a2bfBDb62BE94915C";
+    };
+};
+export declare const baseConfig: {
+    readonly chainId: 8453;
+    readonly nativeToken: {
+        readonly symbol: "ETH";
+        readonly decimals: 18;
+        readonly name: "Ether";
+    };
+    readonly addresses: {
+        readonly commonTokens: {
+            readonly USDC: import("..").Address;
+            readonly USDbC: import("..").Address;
+            readonly DAI: import("..").Address;
+            readonly WETH: import("..").Address;
+            readonly ERC20GAS: import("..").Address;
+        };
+        readonly rTokenDeployments: {
+            readonly testUSD: import("..").Address;
+        };
+        readonly rTokens: {
+            readonly testUSD: import("..").Address;
+        };
+        readonly facadeAddress: import("..").Address;
+        readonly executorAddress: import("..").Address;
+        readonly zapperAddress: import("..").Address;
+    };
+};
+export declare const PROTOCOL_CONFIGS: {
+    usdPriceOracles: {
+        "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913": string;
+        "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA": string;
+        "0x50c5725949a6f0c72e6c4a641f24049a917db0cb": string;
+        "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE": string;
+        "0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22": string;
+    };
+    ethPriceOracles: {
+        "0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22": string;
+    };
+    aave: {
+        tokenWrappers: string[];
+    };
+    stargate: {
+        router: string;
+        wrappers: string[];
+        tokens: string[];
+    };
+    compoundV3: {
+        markets: {
+            baseToken: string;
+            receiptToken: string;
+            vaults: string[];
+        }[];
+    };
+};
+export type BaseConfigType = typeof baseConfig;
+export type BaseUniverse = Universe<BaseConfigType>;
+//# sourceMappingURL=base.d.ts.map

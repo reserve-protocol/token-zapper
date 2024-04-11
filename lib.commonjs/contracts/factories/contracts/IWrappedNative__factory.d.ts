d8785e97f0dc01cc54a2e1366b39da2eb8c8e092
Jan <mig@jankjr.dk>
2024-04-11 21:30:09 +0200

    Release v3.1.9 binaries

---
+++ lib.commonjs/contracts/factories/contracts/IWrappedNative__factory.d.ts
@@ -0,0 +1,38 @@
+import { Signer } from "ethers";
+import type { Provider } from "@ethersproject/providers";
+import type { IWrappedNative, IWrappedNativeInterface } from "../../contracts/IWrappedNative";
+export declare class IWrappedNative__factory {
+    static readonly abi: readonly [{
+        readonly inputs: readonly [{
+            readonly internalType: "address";
+            readonly name: "account";
+            readonly type: "address";
+        }];
+        readonly name: "balanceOf";
+        readonly outputs: readonly [{
+            readonly internalType: "uint256";
+            readonly name: "";
+            readonly type: "uint256";
+        }];
+        readonly stateMutability: "view";
+        readonly type: "function";
+    }, {
+        readonly inputs: readonly [];
+        readonly name: "deposit";
+        readonly outputs: readonly [];
+        readonly stateMutability: "payable";
+        readonly type: "function";
+    }, {
+        readonly inputs: readonly [{
+            readonly internalType: "uint256";
+            readonly name: "amount";
+            readonly type: "uint256";
+        }];
+        readonly name: "withdraw";
+        readonly outputs: readonly [];
+        readonly stateMutability: "nonpayable";
+        readonly type: "function";
+    }];
+    static createInterface(): IWrappedNativeInterface;
+    static connect(address: string, signerOrProvider: Signer | Provider): IWrappedNative;
+}
---
+++ types/configuration/setupERC4626.d.ts
@@ -0,0 +1,3 @@
+import { Universe } from '../Universe';
+export declare const setupERC4626: (universe: Universe, vaultAddr: string[], wrappedToUnderlyingMapping: Record<string, string>) => Promise<void>;
+//# sourceMappingURL=setupERC4626.d.ts.map
